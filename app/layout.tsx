f6082af951a9e49f916a08eb46592ed4e5c44723
30210200 <vraj.dudhat@ucalgary.ca>
2025-03-22 16:18:35 -0600

    imported global styles and core dependencies for layout.tsx file

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import "@/app/globals.css";
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { AppShell } from "@/components/layout/app-shell";
+
+
+const inter = Inter({ subsets: ["latin"] });
