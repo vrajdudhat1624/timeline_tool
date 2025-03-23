ddfdc8d2eaf9d221b9f23976747900dcce83eaad
30210200 <vraj.dudhat@ucalgary.ca>
2025-03-23 12:18:12 -0600

    added a theme selection option for light, dark and system modes.

--- components/layout/mode-toggle.tsx
+++ components/layout/mode-toggle.tsx
@@ -40,3 +40,17 @@ export function ModeToggle() {
     </DropdownMenu>
   )
 }
+
+<DropdownMenuContent align="end">
+  <DropdownMenuItem onClick={() => setTheme("light")}>
+    <Sun className="mr-2 h-4 w-4" />
+    <span>Light</span>
+  </DropdownMenuItem>
+  <DropdownMenuItem onClick={() => setTheme("dark")}>
+    <Moon className="mr-2 h-4 w-4" />
+    <span>Dark</span>
+  </DropdownMenuItem>
+  <DropdownMenuItem onClick={() => setTheme("system")}>
+    <span>System</span>
+  </DropdownMenuItem>
+</DropdownMenuContent>
