8fa8f292906c35defe8da3716990b9f6a9e24877
30210200 <vraj.dudhat@ucalgary.ca>
2025-03-24 18:16:56 -0600

    imported required dependencies for user navigation dropdown

--- components/layout/user-nav.tsx
+++ components/layout/user-nav.tsx
@@ -3,3 +3,16 @@
 export function UserNav() {
   return <div></div>
 }
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { LogOut, User, Settings } from "lucide-react"
