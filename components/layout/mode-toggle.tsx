71fbda5f81006c52d46964c044fcd497c27e9af2
30210200 <vraj.dudhat@ucalgary.ca>
2025-03-24 18:10:20 -0600

    initialized NotificationsDropdown component with state management

--- components/layout/mode-toggle.tsx
+++ components/layout/mode-toggle.tsx
@@ -55,4 +55,3 @@ export function ModeToggle() {
     </DropdownMenu>
   )
 }
-
---
+++ components/layout/notifications-dropdown.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import * as React from "react"
+
+export function NotificationsDropdown() {
+  const [open, setOpen] = React.useState(false)
+
+  return <div></div>
+}
